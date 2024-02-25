24cd1de284b7937d7b76f5a8497ba6baeef64394
nhatthanhdev <phamnhatthanh2212@gmail.com>
2024-02-25 20:59:39 +0700

    hahaha

--- main.js
+++ main.js
@@ -1,13 +1,16 @@
 window.addEventListener('scroll', function() {
     if (window.scrollY > 100) {
         document.querySelector('.header').style.position = 'fixed';
-        document.querySelector('.header').style.backgroundColor = '#030B15';
         document.querySelector('.header').style.paddingTop = "0px";
         document.querySelector('.header').style.paddingBottom = "0px";
+        document.querySelector('.container-header').style.background = '#030B15';
+        document.querySelector('.container-header').style.position = 'fixed';
     } else {
         document.querySelector('.header').style.position = 'relative';
         document.querySelector('.header').style.backgroundColor = 'transparent';
         document.querySelector('.header').style.paddingTop = "60px";
         document.querySelector('.header').style.paddingBottom = "60px";
+        document.querySelector('.container-header').style.backgroundColor = 'transparent';
+        document.querySelector('.container-header').style.position = 'relative';
     }
 });
